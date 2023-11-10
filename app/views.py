f7989650c56da1155388b1ecbfa5eccee207dbd6
sumi1501 <sumithramurugan1501@gmail.com>
2023-11-10 16:37:48 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def anumi(request):
+    return render(request,'anumi.html')
+def anumi1(request):
+    return render(request,'anumi1.html')
+def anumi2(request):
+    return render(request,'anumi2.html')
+def anumi3(request):
+    return render(request,'anumi3.html')
